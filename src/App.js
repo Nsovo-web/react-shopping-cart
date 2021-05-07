dd41d2c2748790610ca1d73fd0a414cc127c4dc8
Nsovo-web <smaserumule@gmail.com>
2021-05-07 10:58:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './images/logo.jpg';
+
+
+function App() {
+  return (
+      <>
+      
+    <div className="grid-container">
+        
+       <header>
+         <a href="/"><img alt={"logo"} src={logo} width="50px"/> Styles</a>
+       </header>
+       <main>
+          products
+       </main>
+       <footer>All Rights Reserved</footer>
+    </div>
+    </>
+  );
+}
+
+export default App;
