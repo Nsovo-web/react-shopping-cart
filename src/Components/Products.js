61ca65aaade2b34c535b852860b9cb671f1747a8
Nsovo-web <smaserumule@gmail.com>
2021-05-11 18:26:52 +0200

    animation modal

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
 import formatCurrency from './../util'
-
+import Fade from 'react-reveal/Fade'
+import Modal from 'react-modal'
+import Zoom from 'react-reveal/Zoom'
 
 export default class Products extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            modProduct:null
+        }
+    }
+    openModal=(product)=> {
+        this.setState({modProduct:product}) //fill the state product with the selected product
+    }
+    closeModal=()=>{
+        this.setState({modProduct:null})
+    }
     render() {
+    // const modProduct = this.state.modProduct;
         return (
             <div>
+                <Fade bottom cascade={true}>
                 <ul className="product-list">
                    {this.props.Products.map((product) =>(
                        <li key={product._id}>
                            <div className="product">
-                            <a href={"#" + product._id}>
+                            <a href={"#" + product._id} onClick={()=>this.openModal(product)}>
                                 <img src={product.image} alt={product.title}></img>
                                 <h3>{product.title}</h3>
                             </a>
@@ -23,6 +39,13 @@ export default class Products extends Component {
                        </li>
                     ))}
                 </ul>
+                </Fade> 
+                    <Zoom >
+                        <Modal isOpen={this.state.modProduct} onRequestClose={this.state.modProduct}>
+                            <button className="close-modal" onClick={()=>this.closeModal}>X</button>
+                        <div>Modal</div>
+                        </Modal>
+                    </Zoom>
             </div>
         )
     }
